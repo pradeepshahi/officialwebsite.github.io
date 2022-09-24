6cfaabd7e28e4ff1934fa1e18a75db99d7aa43ed
Pradeep Shahi <106110065+pradeepshahi@users.noreply.github.com>
2022-09-24 23:31:59 +0545

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-    * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
+    * portfolio - Freelancer v6.0.0 (https://shahipradeep.com/Pradeep Shahi)
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
     */
